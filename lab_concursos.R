6a9bba71361f2d98137eb8d4b7fe5ed52278c4e2
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-03-10 10:13:33 -0300

    Versão inicial do laboratório de concursos

---
+++ lab_concursos.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+
+###### Laboratório para análises de dados de servidores públicos (dados de concurso vindos da tabela cadastro)
+
+busca_dados_cadastro <- function(ano, mes, faz_download= TRUE){
+
+  diretorio_trabalho<-  paste0("data/sevidor_siape",ano,mes)
+
+  if (faz_download){
+    url_base<- sprintf("https://portaldatransparencia.gov.br/download-de-dados/servidores/%s%s_Servidores_SIAPE",ano,mes)
+
+    print(url_base)
+
+
+    download.file(url = url_base, destfile = "data/dado_servidor.zip", mode = "wb")
+
+    unzip(zipfile = "data/dado_servidor.zip",exdir = diretorio_trabalho)
+
+
+  }
+
+
+
+  arquivo_trabalho<- paste0(diretorio_trabalho,"/",ano,mes,"_Cadastro.csv")
+
+  library(readr)
+  janitor::clean_names(read_delim(arquivo_trabalho,
+                                  delim = ";", escape_double = FALSE, locale = locale(encoding = "LATIN1"),
+                                  trim_ws = TRUE))
+
+}
+
+
+siape_201612_Cadastro<- busca_dados_cadastro("2016","12")
